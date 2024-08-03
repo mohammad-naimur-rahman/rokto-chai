763c40f6582737d5756734b911aa807d10f72f72
mohammad-naimur-rahman <naeemhasan28@gmail.com>
2024-08-04 01:40:32 +0600

    add: ga

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { cn } from '@/lib/utils'
+import { GoogleTagManager } from '@next/third-parties/google'
 import type { Metadata } from 'next'
 import { Noto_Sans as FontSans } from 'next/font/google'
 import { Toaster } from 'react-hot-toast'
@@ -21,6 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='bn'>
+      <GoogleTagManager gtmId='GTM-54JF9VLF' />
       <body className={cn('min-h-screen bg-background font-sans antialiased', fontSans.variable)}>
         {children}
         <Toaster position='top-center' />
